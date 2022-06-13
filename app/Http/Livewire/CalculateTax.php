256d34bd0e5c245012db8bff23c80a5ad040fc64
Sanskar Tiwari <sanskar.mk2@gmail.com>
2022-06-13 19:28:47 +0530

    completed calculate tax

--- app/Http/Livewire/CalculateTax.php
+++ app/Http/Livewire/CalculateTax.php
@@ -16,19 +16,35 @@ class CalculateTax extends Component
     public $country = "";
     public $state = "";
     public $city = "";
+    public $price = 0.0;
+    public $tax_city = 0.0;
+    public $tax_country = 0.0;
+    public $tax_state = 0.0;
     public $tax_local = 0.0;
     public $tax_global = 0.0;
+
+    public function show()
+    {
+        $this->tax_local = ($this->tax_city / 100) * $this->price;
+        $this->tax_global = (($this->tax_country + $this->tax_state) / 100) * $this->price;
+    }
 
     public function mount()
     {
         $this->products = Product::all()->pluck("name")->implode(",");
-        // $this->countries = implode(",", Tax::whereType("country")->get()->pluck("name"));
-        // $this->states = implode(",", Tax::whereType("states")->get()->pluck("name"));
-        // $this->cities = implode(",", Tax::whereType("city")->get()->pluck("name"));
-        // $this->product = Product::all("name")->first();
-        // $this->country = Tax::whereType("country")->get()->pluck("name")->first();
-        // $this->state = Tax::whereType("state")->get()->pluck("name")->first();
-        // $this->city = Tax::whereType("country")->get()->pluck("name")->first();
+        $this->countries = Tax::whereType("country")->pluck("name")->implode(",");
+        $this->states = Tax::whereType("state")->pluck("name")->implode(",");
+        $this->cities = Tax::whereType("city")->pluck("name")->implode(",");
+
+        $this->product = Product::all()->pluck("name")->first();
+        $this->country = Tax::whereType("country")->pluck("name")->first();
+        $this->state = Tax::whereType("state")->pluck("name")->first();
+        $this->city = Tax::whereType("city")->pluck("name")->first();
+
+        $this->price = Product::all()->pluck("price")->first();
+        $this->tax_country = Tax::whereType("country")->pluck("tax")->first();
+        $this->tax_state = Tax::whereType("state")->pluck("tax")->first();
+        $this->tax_city = Tax::whereType("city")->pluck("tax")->first();
     }
 
     public function render()
--- resources/views/livewire/calculate-tax.blade.php
+++ resources/views/livewire/calculate-tax.blade.php
@@ -2,15 +2,23 @@
     <form wire:submit.prevent="show">
         <x-select-with-label name="product"
             :listdata="$products" />
+        <x-select-with-label name="country"
+            :listdata="$countries" />
+        <x-select-with-label name="state"
+            :listdata="$states" />
+        <x-select-with-label name="city"
+            :listdata="$cities" />
         <x-submit-button text="Calculate Tax" />
     </form>
 
-    {{-- <main class="flex flex-wrap gap-4 mt-4 mb-4">
-        @foreach ($taxes as $value)
+    <main class="flex flex-wrap justify-center gap-4 mt-4 mb-4">
             <div class="p-8 bg-gradient-to-r from-blue-400 to-cyan-300 rounded-xl">
-                <p class="text-xl">{{ $value->name }}({{ $value->type }})</p>
-                <p class="text-4xl">{{ $value->tax }}</p>
+                <p class="text-xl">Product(Price): {{ $product }}({{ $price }})</p>
+                <p>Country(Tax): {{ "$country($tax_country%)" }}</p>
+                <p>State(Tax): {{ "$state($tax_state%)" }}</p>
+                <p>City(Tax): {{ "$city($tax_city%)" }}</p>
+                <p class="text-xl">Local Tax: {{ $tax_local }}</p>
+                <p class="text-xl">Global Tax: {{ $tax_global }}</p>
             </div>
-        @endforeach
-    </main> --}}
+    </main>
 </div>
